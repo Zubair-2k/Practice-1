c7b84b81e850bf169d8c616f40107c6d6f8f7e27
Zubair-2k <zubair2kdeveloper@gmail.com>
2024-11-19 18:05:35 +0530

    Changes in the E-commerce Website

--- Javascript/DOM Manipulation/e_com Website/index.js
+++ Javascript/DOM Manipulation/e_com Website/index.js
@@ -1,4 +1,8 @@
 var sidebar = document.querySelector(".sidebar");
+var search =document.querySelector("#searchInputBox");
+var products = document.querySelector(".products_display");
+var products_list = products.querySelectorAll("div");
+var noDataFound = document.querySelector(".noDataFound")
     
 function showSidebar(){
     sidebar.style.left="0"
@@ -6,4 +10,31 @@ function showSidebar(){
 
 function closeSidebar(){
     sidebar.style.left="-60%"
+}
+
+function searchImg(event){
+
+    var enteredValue = event.target.value.toUpperCase()
+
+    let filteredCount = 0
+    
+    for(let count=0;count<products_list.length;count++)
+    {
+        var productname=products_list[count].querySelector("p").textContent
+
+        if(productname.toUpperCase().indexOf(enteredValue)<0){
+            products_list[count].style.display="none"
+        }
+        else{
+            products_list[count].style.display="block"
+            filteredCount++
+        }
+    }
+
+    if(filteredCount<=0){
+        noDataFound.style.display="block"
+    }
+    else{
+        noDataFound.style.display="none"
+    }
 }
